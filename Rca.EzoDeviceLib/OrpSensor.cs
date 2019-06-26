0d3127b71682b0c2ff76cf742a4e95d7d16f4f8c
Elias Ruemmler <elias.ruemmler@de.bosch.com>
2019-06-26 12:15:24 +0200

    Add OrpSensor

--- Rca.EzoDeviceLib/OrpSensor.cs
+++ Rca.EzoDeviceLib/OrpSensor.cs
@@ -11,25 +11,18 @@ using Rca.EzoDeviceLib.Specific.Ph;
 namespace Rca.EzoDeviceLib
 {
     /// <summary>
-    /// Represents a pH sensor connected to an Atlas Scientific EZO™ pH Circuit.
-    /// https://www.atlas-scientific.com/product_pages/circuits/ezo_ph.html
+    /// Represents a ORP sensor connected to an Atlas Scientific EZO™ ORP circuit.
+    /// ORP stands for oxidation/reduction potential.
+    /// https://www.atlas-scientific.com/_files/_datasheets/_circuit/ORP_EZO_datasheet.pdf
     /// </summary>
-    public class PhSensor : BaseDevice
+    public class OrpSensor : BaseDevice
     {
         #region Constants
-        private const int DEFAULT_ADDRESS = 0x63; //pH EZO
+        private const int DEFAULT_ADDRESS = 0x62; //ORP EZO
 
         #endregion Constants
 
         #region Properties
-        /// <summary>
-        /// Get or set the temperatur for internal temperatur compensation.
-        /// </summary>
-        public double TemperatureCompensation
-        {
-            get => GetTemperatureCompensation();
-            set => SetTemperatureCompensation(value);
-        }
 
         #endregion Properties
 
@@ -37,8 +30,8 @@ namespace Rca.EzoDeviceLib
         /// <summary>
         /// Constructing this object will initialize it.
         /// </summary>
-        /// <param name="slaveAddress">I2C slave address of EZO device</param>
-        public PhSensor(byte slaveAddress = DEFAULT_ADDRESS) : base(slaveAddress)
+        /// <param name="slaveAddress">I2C slave address of EZO™ device</param>
+        public OrpSensor(byte slaveAddress = DEFAULT_ADDRESS) : base(slaveAddress)
         {
 
         }
@@ -49,67 +42,30 @@ namespace Rca.EzoDeviceLib
         /// <summary>
         /// Get measvalue from a single reading.
         /// </summary>
-        /// <param name="temperature">Optional: Temperatur for internal temperatur compensation.</param>
-        /// <returns>pH measvalue</returns>
-        public double GetMeasValue(double temperature = double.NaN)
+        /// <returns>ORP measvalue</returns>
+        public double GetMeasValue()
         {
-            if (double.IsNaN(temperature))
                 WriteCommand("R");
-            else
-                WriteCommand($"RT,{temperature.ToString("F2", CultureInfo.InvariantCulture)}");
             SpinWait.SpinUntil(() => false, 900);
 
             return ReadDouble();
         }
 
-        /// <summary>
-        /// Get slope information from characteristic curve
-        /// </summary>
-        /// <returns></returns>
-        public Slope GetSlope()
-        {
-            WriteCommand("Slope,?");
-            SpinWait.SpinUntil(() => false, PROCESSING_DELAY);
-
-            var response = ReadResponse(ResponseFormat.DataWithCommand);
-            if (response.IsSuccessful)
-            {
-                try
-                {
-                    return Slope.FromResponseData(response.Data);
-                }
-                catch (Exception ex)
-                {
-                    throw new EzoResponseException(response.Code, "Invalid data", ex);
-                }
-            }
-            else
-                throw new EzoResponseException(response.Code);
-        }
-
         #region Calibration
         /// <summary>
         /// Set a new calibration point.
-        /// Issuing  the  <code>SetCalibrationPoint(EzoPhCalPoint.Mid, x.xx)</code>  command  after
-        /// the EZO pH circuit has been calibrated, will clear  the other  calibration points. Full
-        /// calibration will have to be redone. 
+        /// The EZO™ ORP circuit can be calibrated to any known ORP value. 
         /// </summary>
-        /// <param name="point">Calibration range</param>
-        /// <param name="value">pH value of the buffer solution</param>
-        public void SetCalibrationPoint(CalPoint point, double value)
+        /// <param name="value">setvalue</param>
+        public void SetCalibrationPoint(int value)
         {
-            var cmd = new StringBuilder("cal,");
-            cmd.Append(point.ToString().ToLower());
-            cmd.Append(",");
-            cmd.Append(value.ToString("F2", CultureInfo.InvariantCulture));
-
-            WriteCommand(cmd.ToString());
+            WriteCommand($"cal,{value}");
             SpinWait.SpinUntil(() => false, 900);
             ReadAck(); //throws exception if failed
         }
 
         /// <summary>
-        /// Clear all claibration data from the EZO device.
+        /// Clear all claibration data from the EZO™ device.
         /// </summary>
         public void ClearCalibration()
         {
@@ -174,6 +130,13 @@ namespace Rca.EzoDeviceLib
                 if (expBytes > 0)
                     throw new ArgumentException("Mismatch between fetched and expected calibration data.");
 
+                WriteCommand("Export");
+                SpinWait.SpinUntil(() => false, PROCESSING_DELAY);
+                var responseDone = ReadResponse(ResponseFormat.Data);
+
+                if (responseDone.Data.Length < 1 | !string.Equals(responseDone.Data[0], "*DONE", StringComparison.OrdinalIgnoreCase))
+                    throw new ArgumentException("No confirmation of completed export received.");
+
                 return calibData;
             }
             catch (Exception ex)
@@ -219,32 +182,6 @@ namespace Rca.EzoDeviceLib
 
         #region Internal services
 
-        private void SetTemperatureCompensation(double temperature)
-        {
-            WriteCommand($"L,{temperature.ToString("F2", CultureInfo.InvariantCulture)}");
-            SpinWait.SpinUntil(() => false, PROCESSING_DELAY);
-        }
-
-        private double GetTemperatureCompensation()
-        {
-            WriteCommand("T,?");
-            SpinWait.SpinUntil(() => false, PROCESSING_DELAY);
-
-            var response = ReadResponse(ResponseFormat.DataWithCommand);
-            if (response.IsSuccessful)
-            {
-                try
-                {
-                    return double.Parse(response.Data[0]);
-                }
-                catch (Exception ex)
-                {
-                    throw new EzoResponseException(response.Code, "Invalid data", ex);
-                }
-            }
-            else
-                throw new EzoResponseException(response.Code);
-        }
 
         #endregion Internal services
     }
