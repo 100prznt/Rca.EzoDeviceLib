43e6a1af44c5efa84275e6de66954edab4e5ddf3
Vlada <tvlada73@outlook.com>
2019-09-23 12:51:39 +0200

    Events

---
+++ Rca.EzoDeviceLib/Events/RaiseEventHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rca.EzoDeviceLib.Events
+{
+    internal static class RaiseEventHelper
+    {
+        public async static void CheckRaiseEventOnUIThread(object sender, EventHandler eventHandler, bool raiseEventsOnUIThread)
+        {
+            if (!raiseEventsOnUIThread)
+            {
+                eventHandler?.Invoke(sender, EventArgs.Empty);
+            }
+            else
+            {
+                var dispatcher = CoreApplication.MainView?.CoreWindow?.Dispatcher;
+                if (dispatcher != null)
+                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => eventHandler?.Invoke(sender, EventArgs.Empty));
+                else
+                    eventHandler?.Invoke(sender, EventArgs.Empty);
+            }
+        }
+
+        public async static void CheckRaiseEventOnUIThread<T>(object sender, EventHandler<T> eventHandler, T args, bool raiseEventsOnUIThread)
+            where T : EventArgs
+        {
+            if (!raiseEventsOnUIThread)
+            {
+                eventHandler?.Invoke(sender, args);
+            }
+            else
+            {
+                var dispatcher = CoreApplication.MainView?.CoreWindow?.Dispatcher;
+                if (dispatcher != null)
+                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => eventHandler?.Invoke(sender, args));
+                else
+                    eventHandler?.Invoke(sender, args);
+            }
+        }
+    }
+}
