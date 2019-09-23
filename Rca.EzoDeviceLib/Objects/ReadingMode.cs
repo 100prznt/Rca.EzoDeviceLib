d2cba48b6c2ceabdbef72e35cdbbf37a3b80e1b4
Vlada <tvlada73@outlook.com>
2019-09-23 12:53:33 +0200

    Create ReadingMode.cs

---
+++ Rca.EzoDeviceLib/Objects/ReadingMode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rca.EzoDeviceLib.Objects
+{
+    /// <summary>
+    /// Mod for timed reading
+    /// </summary>
+    public enum ReadingMode
+    {
+        /// <summary>
+        /// Continuous timed reading
+        /// </summary>
+        Continuous,
+
+        /// <summary>
+        /// Manual reading
+        /// </summary>
+        Manual
+    }
+}
